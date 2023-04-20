4996c6965665e4556fa350081225c9a120ae6af8
Ivan <90831802+IvanAwesome@users.noreply.github.com>
2023-04-20 15:59:22 +0100

    general fix commit

---
+++ Assets/Scripts/FlickerControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerControl : MonoBehaviour
+{
+   public bool isFlickering = false;
+    public float timeDelay;
+    void Update()
+    {
+        if (isFlickering == false)
+        {
+            StartCoroutine(FlickeringLight());
+        }
+    }
+    IEnumerator FlickeringLight()
+    {
+        isFlickering = true;
+        this.gameObject.GetComponent<Light>().enabled = true;
+        timeDelay= Random.Range(0f, 90f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = false;
+        timeDelay = Random.Range(1f, 2f);
+        yield return new WaitForSeconds(timeDelay);
+        isFlickering= false;
+    }
+}
