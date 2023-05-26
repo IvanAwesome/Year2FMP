08b33ad2a15407ba4c4fa171ec67d9316ac9611f
Ivan <90831802+IvanAwesome@users.noreply.github.com>
2023-05-26 16:13:03 +0100

    WebGL commit
    might need a bit of fixing

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -5,7 +5,7 @@ public class ButtonScript : MonoBehaviour
 {
     public float scaleAmount = 1.1f;
     public Vector3 originalScale;
-    public string Level0; // Name of the next scene to load
+    public int LevelToLoad; // Name of the next scene to load
 
     private void Start()
     {
@@ -22,7 +22,7 @@ public class ButtonScript : MonoBehaviour
         transform.localScale = originalScale;
 
         // Load the next scene
-        SceneManager.LoadScene("Level 0");
+        SceneManager.LoadScene(LevelToLoad);
         PlayerCoin.totalcoin = 0; 
 
     }
